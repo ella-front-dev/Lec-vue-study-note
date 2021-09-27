1259cded0e6aca29b5634a04f536094bd35d3686
AE201102 <ejy0223@etoos.com>
2021-09-27 14:47:33 +0900

    [etc] 추가 마크업 및 스타일링 추가

--- src/components/common/AppHeader.vue
+++ src/components/common/AppHeader.vue
@@ -1,12 +1,14 @@
 <template>
   <header>
     <div>
-      <router-link to="/" class="logo"> TIL </router-link>
+      <router-link to="/" class="logo">
+        TIL
+        <span v-if="isUserLogin">by {{ $store.state.username }}</span>
+      </router-link>
     </div>
     <div class="navigations">
       <!-- 1 -->
       <template v-if="isUserLogin">
-        <span class="username">{{ $store.state.username }}</span>
         <a href="javascript:;" @click="logoutUser">로그아웃</a>
       </template>
       <!-- 2 -->
@@ -35,6 +37,9 @@ export default {
 </script>
 
 <style scoped>
+.username {
+  color: white;
+}
 header {
   display: flex;
   justify-content: space-between;
@@ -65,11 +70,11 @@ a.logo {
   top: 0;
   width: 100%;
 }
+.logout-button {
+  font-size: 14px;
+}
 a.router-link-exact-active {
   color: white;
   font-weight: bold;
-}
-.username {
-  color: #fff;
 }
 </style>
