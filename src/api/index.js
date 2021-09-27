b9a279aef8a6e6603223232cda84730331368e7d
AE201102 <ejy0223@etoos.com>
2021-09-27 14:48:57 +0900

    [feature] axios interceptor 설정 및 Api 모듈화 작업

--- src/api/index.js
+++ src/api/index.js
@@ -1,13 +1,28 @@
 import axios from 'axios';
+import { setInterceptors } from './common/interceptors';
 
-const instance = axios.create({
-  baseURL: process.env.VUE_APP_API_URL,
-});
+// axiox 초기화 함수
+function createInstance() {
+  const instance = axios.create({
+    baseURL: process.env.VUE_APP_API_URL,
+  });
+  return setInterceptors(instance);
+}
 
+const instance = createInstance();
+
+/**
+ * 회원가입 API
+ * @param {Object} userData username, password, nickname 값
+ */
 function registerUser(userData) {
   return instance.post('/signup', userData);
 }
 
+/**
+ * 로그인 API
+ * @param {Object} userData username, password 값
+ */
 function loginUser(userData) {
   return instance.post('/login', userData);
 }
--- src/components/LoginForm.vue
+++ src/components/LoginForm.vue
@@ -55,6 +55,8 @@ export default {
         };
         const { data } = await loginUser(userData);
         // 메인 페이지로 이동
+        console.log(data.token);
+        this.$store.commit('setToken', data.token);
         this.$store.commit('setUsername', data.user.username);
         this.$router.push('/main');
         //this.logMessage = `${data.user.username} 님이 환영합니다.`;
