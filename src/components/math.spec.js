c335f3d36fd17f6e52f3eb32b130b8e164693ab2
AE201102 <ejy0223@etoos.com>
2021-09-30 13:23:55 +0900

    [feature] JS 기본 테스트 예시

---
+++ src/components/math.spec.js
@@ -0,0 +1,10 @@
+import { sum } from './math';
+
+// sum(10, 20);
+
+describe('math.js', () => {
+  test('10 + 20 = 30', () => {
+    const result = sum(10, 20);
+    expect(result).toBe(30);
+  });
+});
